91a1a542c04f757f61617d789834176737fd3259
raghav_vc <vc.raghav@gmaill.com>
2017-04-24 20:14:43 +0530

    Added an application helper to find the flipkart product id from the link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def get_id link
+    ind = link.index("pid=")
+    ind += 4
+    str = ""
+    i = ind.to_i
+    for i in ind..(ind+15)
+      str += link[i]
+    end
+    str
+  end  
 end
