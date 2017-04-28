8fcba64867b5c150390c331fce160b93897cf002
raghav_vc <vc.raghav@gmaill.com>
2017-04-28 14:55:32 +0530

    Styled the user sign up page and added the logout navbar to the index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'registrations' }
+  
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'products#get_product'
   get '/rake_task/:id' => 'products#rake_task'
+  
 end
